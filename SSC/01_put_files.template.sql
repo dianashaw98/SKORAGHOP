d9582d422f0f013b4c17a9cbe0adc38a5c2c2560
dianashaw98 <153125645+dianashaw98@users.noreply.github.com>
2025-01-25 10:54:55 -0500

    Update 01_put_files.template.sql

--- SSC/01_put_files.template.sql
+++ SSC/01_put_files.template.sql
@@ -4,32 +4,19 @@ PUT file:///{{ env.CI_PROJECT_DIR }}/solution/notebook/SKO_RAG_HOP_SOLUTION.ipyn
 PUT file:///{{ env.CI_PROJECT_DIR }}/solution/notebook/environment.yml @{{ env.DATAOPS_DATABASE }}.hop.rag auto_compress = false overwrite = true;
 
 -- pdf
-PUT file:///{{ env.CI_PROJECT_DIR }}/solution/pdf/AccelerateCodeConversion.pdf @{{ env.DATAOPS_DATABASE }}.hop.rag auto_compress = false overwrite = true;
-PUT file:///{{ env.CI_PROJECT_DIR }}/solution/pdf/AdaptiveQueryOptimization.pdf @{{ env.DATAOPS_DATABASE }}.hop.rag auto_compress = false overwrite = true;
-PUT file:///{{ env.CI_PROJECT_DIR }}/solution/pdf/AggregationPlacement.pdf @{{ env.DATAOPS_DATABASE }}.hop.rag auto_compress = false overwrite = true;
 PUT file:///{{ env.CI_PROJECT_DIR }}/solution/pdf/ArcticEmbedMultilingual.pdf @{{ env.DATAOPS_DATABASE }}.hop.rag auto_compress = false overwrite = true;
-PUT file:///{{ env.CI_PROJECT_DIR }}/solution/pdf/AugmentingNetworkConcurrency.pdf @{{ env.DATAOPS_DATABASE }}.hop.rag auto_compress = false overwrite = true;
-PUT file:///{{ env.CI_PROJECT_DIR }}/solution/pdf/AutomatedQueryRetries.pdf @{{ env.DATAOPS_DATABASE }}.hop.rag auto_compress = false overwrite = true;
-PUT file:///{{ env.CI_PROJECT_DIR }}/solution/pdf/AutomaticClustering.pdf @{{ env.DATAOPS_DATABASE }}.hop.rag auto_compress = false overwrite = true;
 PUT file:///{{ env.CI_PROJECT_DIR }}/solution/pdf/BenchmarkingLLMs.pdf @{{ env.DATAOPS_DATABASE }}.hop.rag auto_compress = false overwrite = true;
 PUT file:///{{ env.CI_PROJECT_DIR }}/solution/pdf/BridgingNeuroscienceAI.pdf @{{ env.DATAOPS_DATABASE }}.hop.rag auto_compress = false overwrite = true;
-PUT file:///{{ env.CI_PROJECT_DIR }}/solution/pdf/CloningofLargeDatabases.pdf @{{ env.DATAOPS_DATABASE }}.hop.rag auto_compress = false overwrite = true;
 PUT file:///{{ env.CI_PROJECT_DIR }}/solution/pdf/DocAIBestPractices.pdf @{{ env.DATAOPS_DATABASE }}.hop.rag auto_compress = false overwrite = true;
-PUT file:///{{ env.CI_PROJECT_DIR }}/solution/pdf/ElasticCloudServices.pdf @{{ env.DATAOPS_DATABASE }}.hop.rag auto_compress = false overwrite = true;
 PUT file:///{{ env.CI_PROJECT_DIR }}/solution/pdf/EssentialGuideGenAI.pdf @{{ env.DATAOPS_DATABASE }}.hop.rag auto_compress = false overwrite = true;
 PUT file:///{{ env.CI_PROJECT_DIR }}/solution/pdf/EvaluatingText2SQLAccuracy.pdf @{{ env.DATAOPS_DATABASE }}.hop.rag auto_compress = false overwrite = true;
-PUT file:///{{ env.CI_PROJECT_DIR }}/solution/pdf/FreePoolManagement.pdf @{{ env.DATAOPS_DATABASE }}.hop.rag auto_compress = false overwrite = true;
-PUT file:///{{ env.CI_PROJECT_DIR }}/solution/pdf/GenAILLMs4Dummies.pdf @{{ env.DATAOPS_DATABASE }}.hop.rag auto_compress = false overwrite = true;
-PUT file:///{{ env.CI_PROJECT_DIR }}/solution/pdf/HierarchicalSelectivity.pdf @{{ env.DATAOPS_DATABASE }}.hop.rag auto_compress = false overwrite = true;
 PUT file:///{{ env.CI_PROJECT_DIR }}/solution/pdf/Joins4StarSFSchemas.pdf @{{ env.DATAOPS_DATABASE }}.hop.rag auto_compress = false overwrite = true;
-PUT file:///{{ env.CI_PROJECT_DIR }}/solution/pdf/KnowledgePreservingComputeReduction.pdf @{{ env.DATAOPS_DATABASE }}.hop.rag auto_compress = false overwrite = true;
 PUT file:///{{ env.CI_PROJECT_DIR }}/solution/pdf/LLMInteractiveWorkloads.pdf @{{ env.DATAOPS_DATABASE }}.hop.rag auto_compress = false overwrite = true;
 PUT file:///{{ env.CI_PROJECT_DIR }}/solution/pdf/ModelHotswapping.pdf @{{ env.DATAOPS_DATABASE }}.hop.rag auto_compress = false overwrite = true;
 PUT file:///{{ env.CI_PROJECT_DIR }}/solution/pdf/MultiturnConversation.pdf @{{ env.DATAOPS_DATABASE }}.hop.rag auto_compress = false overwrite = true;
 PUT file:///{{ env.CI_PROJECT_DIR }}/solution/pdf/RAGWithoutAugmentation.pdf @{{ env.DATAOPS_DATABASE }}.hop.rag auto_compress = false overwrite = true;
 PUT file:///{{ env.CI_PROJECT_DIR }}/solution/pdf/SnowflakeCortexAnalystCortexSearch.pdf @{{ env.DATAOPS_DATABASE }}.hop.rag auto_compress = false overwrite = true;
 PUT file:///{{ env.CI_PROJECT_DIR }}/solution/pdf/SPCSHowWeBuiltIt.pdf @{{ env.DATAOPS_DATABASE }}.hop.rag auto_compress = false overwrite = true;
-PUT file:///{{ env.CI_PROJECT_DIR }}/solution/pdf/SPI.pdf @{{ env.DATAOPS_DATABASE }}.hop.rag auto_compress = false overwrite = true;
 PUT file:///{{ env.CI_PROJECT_DIR }}/solution/pdf/TranslationEnAccion.pdf @{{ env.DATAOPS_DATABASE }}.hop.rag auto_compress = false overwrite = true;
 
 -- jpg
